ad5cd71a1f8b0766588748288c38360fdbf291e5
Anushavan <anushavan777@gmail.com>
2021-02-13 12:26:30 +0400

    initial Commit

--- src/componets/Button.js
+++ src/componets/Button.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import CostomSnackbar from './SnackBar';
 
 class ButtonComp extends React.Component {
     state = {
         corrent: 0,
+        name: '',
     };
 
     increment = () => {
@@ -12,20 +15,53 @@ class ButtonComp extends React.Component {
         }
     };
 
+    hover = () => {
+        console.log('button hover');
+    };
+
     decrement = () => {
         if (this.state.corrent > 0) {
             this.setState({ corrent: this.state.corrent - 1 });
+        } else {
+            return <CostomSnackbar />;
         }
     };
+
+    onChangeValue = (e) => {
+        this.setState({ name: e.target.value });
+    };
+
+    addBtn = () => {
+        this.setState({ name: '' });
+        console.log(this.state.name);
+    };
+
     render() {
         return (
             <>
+                <div>
+                    <Input
+                        placeholder="name"
+                        onChange={this.onChangeValue}
+                        value={this.state.name}
+                    />
+                    <CostomSnackbar />
+                    <Button onClick={this.addBtn}>Add</Button>
+                </div>
                 <h1>{this.state.corrent}</h1>
                 <div>
-                    <Button onClick={this.increment} variant="contained">
+                    <Button
+                        onClick={this.increment}
+                        onMouseEnter={this.hover}
+                        variant="contained"
+                    >
                         +
                     </Button>
-                    <Button onClick={this.decrement} variant="contained">
+                    <Button
+                        onClick={this.decrement}
+                        onMouseEnter={this.hover}
+                        variant="contained"
+                    >
                         -
                     </Button>
                 </div>
