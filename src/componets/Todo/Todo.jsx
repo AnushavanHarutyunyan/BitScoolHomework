61797355d1ccfa7cc89e5c4ed72297d12e78c781
Anushavan <anushavan777@gmail.com>
2021-03-06 15:37:12 +0400

    changed Todo.jsx React.PureComponent to React.Component

--- src/componets/Todo/Todo.jsx
+++ src/componets/Todo/Todo.jsx
@@ -6,7 +6,7 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import styles from './todo.module.css';
 import Edite from '../Edite/Edite';
 
-export default class Todo extends React.PureComponent {
+export default class Todo extends React.Component {
     state = {
         tasks: [
             { _id: uuidv4(), value: 1 },
