802b8173c0d9f2919d2822f881d76a5d176a62db
Anushavan <anushavan777@gmail.com>
2021-05-04 18:27:53 +0400

    finaly

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -14,7 +14,10 @@ const reducer = combineReducers({
     contactState: contactReducer,
     searchState: searchReducer,
 });
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 const store = createStore(reducer, applyMiddleware(...middlewares));
 window.store = store;
 
